712eb7b45097e6ccacf2c0ecc6853d37098f2f1c
kaamil2 <kaamil22thobani@gmail.com>
2024-01-07 22:56:54 -0500

    succesfully finished footer

--- components/global/Footer/FooterPreview.tsx
+++ components/global/Footer/FooterPreview.tsx
@@ -11,5 +11,5 @@ type Props = {
 export default function NavbarPreview(props: Props) {
   const { data } = useSettings(props.initial)
 
-  return <FooterLayout data={data!} />
+  return <FooterLayout />
 }
