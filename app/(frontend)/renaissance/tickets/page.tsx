90001dec81264aa681c91bacb0cb9552a8ad8ec8
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-01-16 15:08:22 -0500

    Create tickets page

---
+++ app/(frontend)/renaissance/tickets/page.tsx
@@ -0,0 +1,64 @@
+import { ArrowRightIcon } from 'lucide-react'
+import Link from 'next/link'
+
+const PUBLIC_LINK =
+  'https://nuhuskies.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=GS:ATHLETICS:SL2324:TED0224:&linkID=northeastern-athletics&siteId=ev_northeastern-athletics'
+const STUDENTS_LINK =
+  'https://nuhuskies.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=ST:ATHLETICS:SL2324:TED0224:S3&linkID=northeastern-athletics&shopperContext=ST&groupCode=SL&siteId=ev_northeastern-athletics'
+
+export default function RenaissanceSpeakers() {
+  return (
+    <div className="w-full p-6 font-serif border-y xl:border xl:rounded-md md:p-12 border-renaissance-light bg-renaissance-light/15">
+      <div className="max-w-2xl mx-auto mb-8">
+        <h1 className="text-2xl font-bold text-center md:text-3xl text-renaissance-dark">
+          Tickets
+        </h1>
+      </div>
+
+      <div className="grid max-w-2xl gap-4 mx-auto mb-8 md:grid-cols-2">
+        <div className="flex flex-col justify-between p-8 rounded-md bg-renaissance-light text-renaissance-dark">
+          <div>
+            <h2 className="mb-4 text-3xl font-bold">Northeastern Students</h2>
+            <p className="leading-snug">
+              Discounted student tickets are available to for $10. Be sure to
+              log in with your Northeastern credentials.
+            </p>
+          </div>
+
+          <div>
+            <Link
+              href={STUDENTS_LINK}
+              className="inline-flex items-center gap-2 px-6 py-2 mt-8 mb-3 text-xl uppercase transition-colors rounded-full text-renaissance-light font-renaissance bg-renaissance-dark w-max hover:bg-black"
+            >
+              Purchase for $10 <ArrowRightIcon size={16} />
+            </Link>
+            <p className="ml-5 leading-snug uppercase font-renaissance">
+              *Limit one (1) per person
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between p-8 rounded-md bg-renaissance-dark text-renaissance-light">
+          <div>
+            <h2 className="mb-4 text-3xl font-bold">General Public</h2>
+            <p className="leading-snug">
+              Tickets are available to the general public for $15 each.
+            </p>
+          </div>
+
+          <div>
+            <Link
+              href={PUBLIC_LINK}
+              className="inline-flex items-center gap-2 px-6 py-2 mx-auto mt-8 mb-3 text-xl uppercase transition-colors rounded-full text-renaissance-dark font-renaissance bg-renaissance-light w-max hover:bg-white"
+            >
+              Purchase for $15 <ArrowRightIcon size={16} />
+            </Link>
+            <p className="ml-5 leading-snug uppercase font-renaissance">
+              *Limit two (2) per person
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/(frontend)/renaissance/tickets/route.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export const runtime = 'edge'
-
-export async function GET(request: Request) {
-  redirect(
-    'https://nuhuskies.evenue.net/cgi-bin/ncommerce3/SEGetEventInfo?ticketCode=GS:ATHLETICS:SL2324:TED0224:&linkID=northeastern-athletics&siteId=ev_northeastern-athletics',
-  )
-}
