d3cd8d7ce7c02e2fe2373c2660d9062816724b73
kaamil2 <kaamil22thobani@gmail.com>
2024-05-18 01:45:28 -0400

    added the changes

--- app/(frontend)/(generic)/layout.tsx
+++ app/(frontend)/(generic)/layout.tsx
@@ -8,13 +8,13 @@ export default async function IndexRoute({
 }) {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center w-full max-w-screen-xl gap-4 p-4 mx-auto text-center bg-red-200 border-red-600 border-y xl:my-2 lg:flex-row xl:border xl:rounded-md">
+      <div className="flex flex-col items-center justify-center w-full max-w-screen-xl gap-4 p-4 mx-auto text-center bg-red-300 border-red-800 border-y xl:my-2 lg:flex-row xl:border xl:rounded-md">
         <p className="!m-0 text-pretty">
           <strong>Applications Now Open for TEDxNortheasternU 2024-2025</strong>
         </p>
         <Link
           href="/apply"
-          className="shrink-0 inline-flex items-center gap-2 px-4 py-1.5 text-sm font-bold bg-red-400 transition-colors rounded-full bg-red-600 w-max hover:bg-red-600/85"
+          className="shrink-0 inline-flex items-center gap-2 px-4 py-1.5 text-sm font-bold transition-colors rounded-full bg-red-500/85 w-max hover:bg-red-400"
         >
           Apply Here <ArrowRightIcon size={16} />
         </Link>
