5a7c493497f55c08274e3e31a77407534b1a69d8
TEDx-NortheasternU <tedxneu@gmail.com>
2025-03-27 13:58:25 -0400

    Update page.tsx

--- app/(frontend)/(generic)/nominate/page.tsx
+++ app/(frontend)/(generic)/nominate/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function Page() {
-  redirect('https://airtable.com/appqG2AJwajWIUmWp/shryuSMzpDKQriGbP')
+  redirect('https://airtable.com/appfmGmk4yM44WDKh/shrqqY5VGETe2x7tk')
   return null
 }
